375def6f34190b2f28487a78f0c4dd74beb7820b
Abhishek9267 <200424380@student.georgianc.on.ca>
2020-03-30 18:53:50 -0400

    Completed Final lab

---
+++ FirstMVCdemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstMVCdemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FirstMVCdemo/ViewModels/EmployeeDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FirstMVCdemo.Models;
+namespace FirstMVCdemo.ViewModels
+{
+    public class EmployeeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+        public Address Address { get; set; }
+        public string PageTitle { get; set; }
+        public string PageHeader { get; set; }
+    }
+}
